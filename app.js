3fe74c690c4307103f6ef7cfc381b03967f58221
Kenouly Lo Van <kenouly.lovan@gmail.com>
2020-10-08 17:04:59 +0200

    turned db connection uri

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const Mongostore = require('connect-mongo')(session)
 const moment = require("moment");
 
 mongoose
-  .connect('mongodb://localhost/module-2-project', {useNewUrlParser: true})
+  .connect(process.env.MONGO_URI, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
